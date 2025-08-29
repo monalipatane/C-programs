a9e2dcd5179683d96c1ee671e0b31f631609b95b
monalipatane <monalipatane05@gmail.com>
2025-08-29 10:33:00 +0530

    Update if_statement.c

--- if_statement.c
+++ if_statement.c
@@ -3,26 +3,22 @@
 int main() {
     int num, marks;
 
-    // user input
     printf("Enter a number: ");
     scanf("%d", &num);
 
     printf("Enter student percentage: ");
     scanf("%d", &marks);
 
-    // Simple if
     if (num > 10) {
         printf("1.Simple if:Number is greater than 10\n");
     }
 
-    // if-else
     if (num % 2 == 0) {
         printf("2.if-else:Number is even\n");
     } else {
         printf("2.if-else:Number is odd\n");
     }
 
-    // nested if-else
     if (num > 0) {
         if (num < 20) {
             printf("3.nested if-else:Number is positive and less than 20\n");
@@ -33,7 +29,6 @@ int main() {
         printf("3.nested if-else:Number is zero or negative\n");
     }
 
-    // else-if ladder
     printf("4.else-if ladder:Grade based on precentage (%d): ", marks);
     if (marks >= 90) {
         printf("A\n");
